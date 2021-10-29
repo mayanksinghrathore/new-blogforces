58fd62517bf699779f79c62be70bf2e52b6f98c1
Mayank Singh Rathore <66118938+mayanksinghrathore@users.noreply.github.com>
2021-10-29 13:58:30 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");//google api for css
 
 * {
   margin: 0;
